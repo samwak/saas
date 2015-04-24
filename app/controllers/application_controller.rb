9b4bc64abb87e6f68ec03cf7566e3518001a2c99
Sam Wak <samuelwaknine@gmail.com>
2015-04-25 01:18:33 +0200

    Fixed typo in Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ before_filter :configure_permitted_parameters, if: :devise_controller?
   protected
 
   	def configure_permitted_parameters
-  		devise_parameters_sanitizier.for(:sign_up) { 
+  		devise_parameter_sanitizer.for(:sign_up) { 
   			|u| u.permit(:name, :stripe_card_token, :email, :password, :password_configartion)
   		}
   	end
