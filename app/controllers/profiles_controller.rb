f82b27f9388a04276a800629b1d4bcdf509f25f2
Sam Wak <samuelwaknine@gmail.com>
2015-04-27 22:32:26 +0200

    Added befor filters to check correct user

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+	# On ne peut pas acceder a aucune des pages du profiles si on est pas connecte comme le user en question
+before_action :authenticate_user!
+before_action :only_current_user
 
 	def new
 		#form where a user can fill their OWN profile
@@ -26,11 +29,27 @@ class ProfilesController < ApplicationController
 		@profile = @user.profile
 	end
 
+
+	def update
+		@user = User.find(params[:user_id])
+		@profile = @user.profile
+
+		if @profile.update_attributes(profile_params)
+			flash[:success] = 'Profile Updated'
+			redirect_to user_path(params[:user_id])
+		else
+			render action: :edit
+		end
+	end
+
 	private
 		def profile_params
 			params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+		end
 
-
+		def only_current_user
+			@user = User.find(params[:user_id])
+			redirect_to(root_url) unless @user == current_user
 		end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!
+	before_action :only_current_user
 
 	def show
 		#Regarder dans l'url pour savoir le param demande
 		@user =User.find(params[:id])
 	end
+
+	def only_current_user
+			@user = User.find(params[:id])
+			redirect_to(root_url) unless @user == current_user
+		end
 end
