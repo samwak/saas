fe7c3f3e50251ace79a1f154feb44bc97ab9678d
Sam Wak <samuelwaknine@gmail.com>
2015-04-27 21:54:08 +0200

    Home page approvments

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,7 +4,7 @@ class ProfilesController < ApplicationController
 		#form where a user can fill their OWN profile
 		#recupere le user qui est actuellement log
 		@user = User.find(params[:user_id])
-		@profile = @user.build_profile
+		@profile = Profile.new
 	end
 
 	def create
